2767e51d35a656381a59f04b7e5f97f28d87d7ef
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-17 13:05:42 +0900

    A-Level2 追加

---
+++ A-Level2.sql
@@ -0,0 +1,124 @@
+-- 9
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	口座番号 = '0037651';
+
+-- 10
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	残高 > 0;
+
+-- 11
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	口座番号 > '1000000';
+
+-- 12
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	更新日 < '2017-01-01';
+
+-- 13
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	残高 >= '1000000';
+
+-- 14
+SELECT
+	*
+FROM
+	口座
+WHERE NOT 
+	種別 = '1';
+
+-- 15
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	更新日 IS NULL;
+
+-- 16
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	名義 like '%ハシ%';
+
+-- 17
+SELECT
+	*
+FROM
+	口座
+WHERE 
+	更新日 
+BETWEEN
+	'2018-01-01' AND '2018-01-31';
+
+-- 18
+SELECT
+	*
+FROM
+	口座
+WHERE NOT
+	種別 = '1';
+
+-- 19
+SELECT 
+	*
+FROM
+	口座
+WHERE
+	名義 in ('サカタ リョウヘイ', 'マツモト ミワコ', 'ハマダ サトシ');
+
+-- 20
+SELECT 
+	*
+FROM
+	口座
+WHERE
+	更新日
+BETWEEN
+	'2017-12-30' AND '2018-01-04';
+
+-- 21
+SELECT
+	*
+FROM
+	口座
+WHERE
+	残高 < 10000
+AND
+	更新日 IS NOT NULL;
+
+-- 22
+SELECT
+	*
+FROM
+	口座
+WHERE
+	口座番号
+BETWEEN '2000000' AND '2999999'
+AND 名義 LIKE 'エ__ %コ'; 
+
+-- 23
+口座テーブル：口座番号
+取引テーブル：取引番号
+取引事由テーブル：取引事由id
