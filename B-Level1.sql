3b94c14de60895735610948e4ed3be7d1bb4b25a
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-17 18:21:05 +0900

    B-Level1 修正

--- B-Level1.sql
+++ B-Level1.sql
@@ -25,12 +25,12 @@ FROM
 -- 5
 INSERT INTO
 	商品
-VALUES('W0461', '冬のあったかコート', 12800, 1);
+VALUES('W0461', '冬のあったかコート', 12800, 1, NULL);
 
 INSERT INTO
 	商品
-VALUES('S0331', '春のさわやかコート', 6800, 1);
+VALUES('S0331', '春のさわやかコート', 6800, 1, NULL);
 
 INSERT INTO
 	商品
-VALUES('A0582', '秋のシックなコート', 9800, 1);
+VALUES('A0582', '秋のシックなコート', 9800, 1, NULL);
