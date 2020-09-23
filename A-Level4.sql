3f6e4c07d4eadef36c83ecbfc0b415e4ac137431
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-23 14:36:55 +0900

    A-Level4 修正

--- A-Level4.sql
+++ A-Level4.sql
@@ -1,8 +1,7 @@
 -- 34
 SELECT 
 	口座番号,
-	残高,
-	ROUND(残高, -3) AS 千円単位の残高
+	残高 / 1000 AS 千円単位の残高
 FROM
 	口座
 WHERE
@@ -26,6 +25,8 @@ UPDATE
 	口座
 SET 
 	残高 = (残高 - 3000) * 1.003;
+WHERE
+	口座番号 IN('0652281', '1026413', '2239710');
 
 -- 37
 SELECT
@@ -35,19 +36,19 @@ SELECT
 FROM
 	口座
 WHERE
-	更新日 < '2016-01-01';
+	更新日 < '2017-01-01';
 
 --38
 SELECT
 	口座番号,
-	'カ' || 名義 
+	'カ）' || 名義 
 FROM
 	口座
 WHERE
 	種別 = '3';
 
 -- 39
-SELECT
+SELECT DISTINCT
 	種別 AS 種別コード
 	CASE
 	WHEN 種別 = '1' THEN '普通'
@@ -69,11 +70,10 @@ SELECT
 FROM
 	口座;
 
--- 41
 SELECT
 	LENGTH(口座番号),
 	LENGTH(REPLACE(名義, ' ', '')),
-	LENGTH(残高)
+	LENGTH(CAST(残高 AS VARCHAR))
 FROM
 	口座;
 
@@ -91,15 +91,15 @@ SELECT
 FROM
 	口座
 WHERE
-	LENGTH(残高) >= 4
+	LENGTH(CAST(残高 AS VARCHAR)) >= 4
 AND
-	SUBSTRING(残高, LENGTH(残高) - 3, LENGTH(残高)) LIKE '%000';
+	SUBSTRING(CAST(残高 AS VARCHAR), LENGTH(CAST(残高 AS VARCHAR)) - 2, 3) = '000';
 
 -- 44
 SELECT
 	口座番号,
 	残高,
-	ROUND(残高 * 1.002, 1) AS 利息
+	TRUNC(残高 * 1.0002, 1) AS 利息
 FROM
 	口座
 ORDER BY
@@ -110,9 +110,9 @@ SELECT
 	口座番号,
 	残高,
 	CASE
-	WHEN 残高 < 500000 THEN ROUND(残高 * 0.01, 1)
-	WHEN 残高 BETWEEN 500000 AND 1999999 THEN ROUND(残高 * 0.02, 1)
-	WHEN 残高 >= 2000000 THEN ROUND(残高 * 0.03, 1)
+	WHEN 残高 < 500000 THEN TRUNC(残高 * 0.0001, 1)
+	WHEN 残高 BETWEEN 500000 AND 1999999 THEN TRUNC(残高 * 0.0002, 1)
+	WHEN 残高 >= 2000000 THEN TRUNC(残高 * 0.0003, 1)
 	END AS 残高別利息
 FROM
 	口座
@@ -134,13 +134,12 @@ INSERT INTO(口座番号, 名義, 種別, 残高, 更新日)
 VALUES('1739298', 'ホシノ サトミ', '1', 704610, CURRENT_DATE);
 
 -- 47
-SELECT
-	*,
-	REPLACE(更新日, '%-%-%' '%年%月%日')
-FROM
-	口座
-WHERE
-	更新日 >= '2018-01-01';
+SELECT 口座番号, 名義, 種別, 残高,
+       SUBSTRING(CAST(更新日 AS VARCHAR), 1, 4) || '年' ||
+       SUBSTRING(CAST(更新日 AS VARCHAR), 6, 2) || '月' ||
+       SUBSTRING(CAST(更新日 AS VARCHAR), 9, 2) || '日' AS 更新日
+  FROM 口座
+ WHERE 更新日 >= '2018-01-01'
 
 -- 48
 SELECT
